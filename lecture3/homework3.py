ad8f88a236dcd0b7a982048a0cc6bdcdd3bc6ab0
Ivan138v <72059132+Ivan138v@users.noreply.github.com>
2020-10-12 20:29:30 +0700

    Create homework3.py

---
+++ lecture3/homework3.py
@@ -0,0 +1,8 @@
+Celsius = int(input("Enter a Temperature in Celsius: "))
+Fahrenheit = ((9 * Celsius)/5) + 32
+def convert(Celsius, Fahrenheit):
+    """'convert' function takes 2 parameters, 'Celsius' & 'Fahrenheit' and
+        completes the operation for the two variables"""
+    print(Celsius, "in degrees Celsius is", Fahrenheit, "in degrees Fahrenheit")
+    return Fahrenheit
+convert(Celsius, Fahrenheit)
